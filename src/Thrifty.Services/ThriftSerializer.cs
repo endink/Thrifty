7e728a2e8805bbb60a183bf7ac247c56dbc5d814
itbencn <itben@vip.qq.com>
2022-11-30 13:40:40 +0800

    add serialization methods

--- src/Thrifty.Services/ThriftSerializer.cs
+++ src/Thrifty.Services/ThriftSerializer.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using Thrift.Protocol;
 using Thrift.Transport;
+using System;
 
 namespace Thrifty.Services
 {
@@ -47,6 +48,62 @@ namespace Thrifty.Services
             return objectBytes;
         }
 
+        public byte[] Serialize(object s, Type type)
+        {
+            byte[] objectBytes = null;
+            var codec = _thriftCodecManager.GetCodec(type) as IThriftCodec;
+            using (var outs = new MemoryStream())
+            {
+                using (var tt = new TStreamTransport(null, outs))
+                {
+                    using (var protocol = this.GetProtocol(tt))
+                    {
+                        codec.WriteObject(s, protocol);
+                        objectBytes = outs.ToArray();
+                    }
+                }
+            }
+            return objectBytes;
+        }
+
+        public byte[] Serialize<TObject>(TObject s, TMessage message)
+        {
+            byte[] objectBytes = null;
+            var codec = _thriftCodecManager.GetCodec(typeof(TObject)) as IThriftCodec;
+            using (var outs = new MemoryStream())
+            {
+                using (var tt = new TStreamTransport(null, outs))
+                {
+                    using (var protocol = this.GetProtocol(tt))
+                    {
+                        protocol.WriteMessageBegin(message);
+                        codec.WriteObject(s, protocol);
+                        objectBytes = outs.ToArray();
+                    }
+                }
+            }
+            return objectBytes;
+        }
+
+        public byte[] Serialize(object s, Type type, TMessage message)
+        {
+            byte[] objectBytes = null;
+            var codec = _thriftCodecManager.GetCodec(type) as IThriftCodec;
+            using (var outs = new MemoryStream())
+            {
+                using (var tt = new TStreamTransport(null, outs))
+                {
+                    using (var protocol = this.GetProtocol(tt))
+                    {
+                        protocol.WriteMessageBegin(message);
+                        codec.WriteObject(s, protocol);
+                        objectBytes = outs.ToArray();
+                    }
+                }
+            }
+            return objectBytes;
+        }
+
         public TObject Deserialize<TObject>(byte[] objectBytes)
         {
             var codec = _thriftCodecManager.GetCodec(typeof(TObject)) as IThriftCodec;
@@ -57,6 +114,53 @@ namespace Thrifty.Services
                     using (var protocol = this.GetProtocol(tt))
                     {
                         return (TObject)codec.ReadObject(protocol);
+                    }
+                }
+            }
+        }
+
+        public object Deserialize(byte[] objectBytes, Type type)
+        {
+            var codec = _thriftCodecManager.GetCodec(type) as IThriftCodec;
+            using (var ins = new MemoryStream(objectBytes))
+            {
+                using (var tt = new TStreamTransport(ins, null))
+                {
+                    using (var protocol = this.GetProtocol(tt))
+                    {
+                        return codec.ReadObject(protocol);
+                    }
+                }
+            }
+        }
+
+        public TObject Deserialize<TObject>(byte[] objectBytes, out TMessage message)
+        {
+            var codec = _thriftCodecManager.GetCodec(typeof(TObject)) as IThriftCodec;
+            using (var ins = new MemoryStream(objectBytes))
+            {
+                using (var tt = new TStreamTransport(ins, null))
+                {
+                    using (var protocol = this.GetProtocol(tt))
+                    {
+                        message = protocol.ReadMessageBegin();
+                        return (TObject)codec.ReadObject(protocol);
+                    }
+                }
+            }
+        }
+
+        public object Deserialize(byte[] objectBytes, Type type, out TMessage message)
+        {
+            var codec = _thriftCodecManager.GetCodec(type) as IThriftCodec;
+            using (var ins = new MemoryStream(objectBytes))
+            {
+                using (var tt = new TStreamTransport(ins, null))
+                {
+                    using (var protocol = this.GetProtocol(tt))
+                    {
+                        message = protocol.ReadMessageBegin();
+                        return codec.ReadObject(protocol);
                     }
                 }
             }
